1495d91bb0505896c640ef9d5edffff012aececd
kavitha <vika2k@yahoo.com>
2024-09-01 13:24:29 +0800

    new sqt fun

--- calc.py
+++ calc.py
@@ -17,6 +17,12 @@ class Calculator:
     def divide(self, a, b):
 
         return a / b
+    
+    # TODO: Implement the following function to calculate the square root of a number.
+
+    def square_root(self, x):   
+
+        return math.sqrt(x)
 if __name__ == "__main__":
 
     calculator = Calculator()
@@ -28,4 +34,6 @@ print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
 print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
 print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
 print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+num3 = 25
 
+print(f"The square root of {num3} = {calculator.square_root(num3)}")
