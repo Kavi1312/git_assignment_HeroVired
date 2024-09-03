5b4370d2de69d0f32e2cfdc54c576286c93d2e02
kavitha <vika2k@yahoo.com>
2024-09-04 00:52:09 +0800

    committed rectangular

--- geomcal.py
+++ geomcal.py
@@ -1,18 +1,15 @@
 import math
 
 class GeometryCalculator:
-
     def calculate_circle_area(self, radius):
-
         return math.pi * radius ** 2
 
     def calculate_rectangle_area(self, length, width):
-
         return length * width
 
 if __name__ == "__main__":
-
     calculator = GeometryCalculator()
+<<<<<<< Updated upstream
 
 # TODO: Implement the feature to calculate the area of a circle
 
@@ -25,3 +22,7 @@ print(f"The area of the circle with radius {radius} = {calculator.calculate_circ
 # width = 6
 
 # print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
+=======
+    radius = 5
+    print(f"The area of the circle with radius {radius} = {calculator.calculate_circle_area(radius)}")
+>>>>>>> Stashed changes
